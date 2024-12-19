a01c7d5b8d0cc88ea293e516ce0c494567be57c5
SridharVadla <vadlasreedhar123@gmail.com>
2024-12-19 13:23:24 +0530

    restuarent module_system

---
+++ restuarent/src/lib.rs
@@ -0,0 +1,9 @@
+mod hosting;
+mod serving;
+
+
+pub fn main () {
+  hosting::add_to_waitlist();
+}
+
+
---
+++ restuarent/src/serving/mod.rs
@@ -0,0 +1,8 @@
+pub fn take_order(){
+  println("colllect the order from the table ");
+}
+
+
+pub fn serve_food(){
+  println(" serving the food to the customer");
+}
