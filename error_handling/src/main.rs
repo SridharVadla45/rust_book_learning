d49d00bc9307b291320eea68a7b110cbafa81094
SridharVadla <vadlasreedhar123@gmail.com>
2024-12-25 11:06:34 +0530

    generic started

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,11 +1,36 @@
+use std::{fs::File, io::ErrorKind};
+
 fn main() {
-    // errors - recoverable errors - Result<T,E> 
-    // unrecoverable errors - panic! macro 
+    // errors - recoverable errors - Result<T,E>
+    // unrecoverable errors - panic! macro
 
+    // unrecoverable errors - panic! macro
+    // panic!("{}","explictly calling panic to interupt the program to exit ".red());
 
+    let list_of_numbers = vec![1, 2, 3, 4];
+    let two = list_of_numbers[2];
+    println!("{two}");
 
-    // unrecoverable errors - panic! macro 
+    let greeting_file = File::open("hello.txt");
+    let hello = match greeting_file {
+        Ok(file) => {
+            println!("File : {file:?}");
+            file
+        }
+        Err(e) => {
+             match e.kind() {
+                ErrorKind::NotFound => match File::create("hello.txt") {
+                    Ok(file) => file,
+                    Err(e) => panic!("{e:?}"),
+                },
+                _ => {
+                    panic!("problem opening the file specified : {e:?}");
+                }
+            }
+            
+            // println!("error :{}",e.to_string().red());
+        }
+    };
 
-    panic!("explicit calling panic to interupt the program to exit ");
-
+    //
 }
